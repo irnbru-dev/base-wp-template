76df02689b1982e784f26468df24bb6ce20e0527
Konovalova <darina.konovalova@it-grad.ru>
2019-03-12 14:22:10 +0300

    fix THEME renaming

--- wp-content/themes/MY_THEME/header.php
+++ wp-content/themes/MY_THEME/header.php
@@ -5,7 +5,7 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Oyster</title>
+    <title>MY_THEME</title>
     <?php wp_head(); ?>
 </head>
 <body>
